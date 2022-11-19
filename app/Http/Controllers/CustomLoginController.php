f699145c4abe931ece4cc5e11668ec00d8a82608
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2022-11-19 18:55:25 +0200

    first commit

---
+++ app/Http/Controllers/CustomLoginController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class CustomLoginController extends Controller
+{
+    public function showRegisterForm()
+    {
+        return view('patient.auth.register');
+    }
+
+    public function registerPatient(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:4',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed',
+            'avatar' => 'nullable|mimes:jpg,jpeg,png,gif,webp|max:30000',
+        ]);
+        if ($validator->fails())
+            return redirect('register/patient')->withErrors($validator)->withInput();
+        $image_name = null;
+        if ($request->has('avatar')) {
+            // return $request;
+            // $image_name = $this->handleImage($request, 'studentImages');
+            $image_name = time() . '.' . $request->file('avatar')->getClientOriginalExtension();
+            $name = $request->file('avatar')->storeAs('patients', $image_name, 'public');
+        }
+        DB::beginTransaction();
+        User::query()->create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+            'avatar' => $image_name,
+        ]);
+        DB::commit();
+        return redirect()->route('login');
+    }
+
+
+    public function showLoginForm()
+    {
+        return view('patient.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if ($validator->fails())
+            return redirect('login/patient')->withErrors($validator)->withInput();
+
+        if (Auth::attempt($this->credentials($request))) {
+            return view('patient.patient-home');
+        }
+        //return $request;
+
+        $email = User::query()->where('email',$request->email)->first();
+        // return $email;
+        if ($email) {
+            // return 'email exists';
+            session()->flash('email','Password Is Incorrect!');
+            return redirect()->back();
+        }
+        session()->flash('email','Email doesnt exist sign up now!');
+        return redirect()->back();
+
+    }
+
+    public function credentials($request)
+    {
+        return $request->only($this->username(), 'password');
+    }
+
+    public function username(): string
+    {
+        return 'email';
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+    public function handleImage($request, $folder): string
+    {
+        $image_name = time() . '.' . $request->get('avatar')->getClientOriginalExtension();
+        $request->file('avatar')->storeAs($folder, $image_name);
+        return $image_name;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\CustomLoginController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Auth::routes();
+
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['middleware' => 'guest:web'], function () {
+    Route::get('register/patient', [CustomLoginController::class, 'showRegisterForm'])->name('register.form');
+    Route::post('register', [CustomLoginController::class, 'registerPatient'])->name('register');
+
+    Route::get('login/patient', [CustomLoginController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [CustomLoginController::class, 'login'])->name('loginIn');
+
+
+
+});
+
+Route::group(['middleware' => 'auth:web'],function() {
+        Route::get('home', function () {
+        return view('patient.patient-home');
+    })->name('home');
+
+
+});
+
